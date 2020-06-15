839c2eeb65a4263664d65550c25e8d10ea2c8048
gnanaprakashcheckin <gnanaprakashcheckin@gmail.com>
2020-06-16 00:32:16 +0530

    Multi threading

---
+++ Multithreads/src/main/java/com/threads/part1/threadsbasics/part2/Main.java
@@ -0,0 +1,37 @@
+package com.threads.part1.threadsbasics.part2;
+
+class MyThread extends Thread {
+	
+	public void run() {
+		
+		for( ;; ) {
+			
+			if (interrupted()) {
+				System.out.println("Thread is interrupted hence stopping..");
+				break;
+			}
+			
+			System.out.print("T");
+		}
+		
+	}
+	
+}
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		MyThread thr = new MyThread();
+		thr.start();
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		thr.interrupt();
+	}
+
+}
---
+++ Multithreads/src/main/java/com/threads/part2/daemonthread/Main.java
@@ -0,0 +1,26 @@
+package com.threads.part2.daemonthread;
+
+class MyTask implements Runnable {
+	
+	@Override
+	public void run() {
+		for (;;) {
+			System.out.print("T");
+		}
+	}
+}
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Thread thr = new Thread(new MyTask());
+		thr.setDaemon(true);
+		thr.start();
+		
+		for (int i=1; i <= 200; i++) {
+			System.out.print(" M ");
+		}
+	}
+}
+
